5dacf7f848eeb7d3689ed6c0e5130cd5982d49d4
Expua1 <badsnake1@gmail.com>
2024-03-10 16:28:37 +0200

    init

--- src/сomponents/ProductList/ProductList.jsx
+++ src/сomponents/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://34.82.221.250/web-data', {
+        fetch('http://34.82.221.250:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
