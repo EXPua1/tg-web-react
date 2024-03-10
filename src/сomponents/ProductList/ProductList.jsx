c7a1e85da06141329e108b42dbf9c1ca83fa8819
Expua1 <badsnake1@gmail.com>
2024-03-10 16:23:19 +0200

    init

--- src/сomponents/ProductList/ProductList.jsx
+++ src/сomponents/ProductList/ProductList.jsx
@@ -1,66 +1,64 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
+import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 13000, description: 'Синего цвета прямые'},
-    {id: '2', title: 'Куртка', price: 3000, description: 'Зеленого цвета теплая'},
-    {id: '3', title: 'Джинсы', price: 2000, description: 'Синего цвета узкие'},
-    {id: '4', title: 'Куртка', price: 12000, description: 'Красного цвета легкая'},
-    {id: '5', title: 'Джинсы', price: 54000, description: 'Синего цвета короткие'},
-    {id: '6', title: 'Куртка', price: 2000, description: 'Оранжевого цвета долгая'},
-    {id: '7', title: 'Джинсы', price: 3000, description: 'Черного цвета прямые'},
-    {id: '8', title: 'Куртка', price: 12000, description: 'Зеленого цвета, битая'},
+    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
-    const getTotalPrice = (items = []) => {
-        return items.reduce((acc, item) => {
-            return acc += item.price
-        }, 0)
-    }
-    const ProductList = () => {
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
+
+const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-
-
-
     const {tg, queryId} = useTelegram();
 
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId,
+        }
+        fetch('http://34.82.221.250/web-data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }, [addedItems])
 
-        const onSendData = useCallback(()=>{
-            const data = {
-                products: addedItems,
-                totalPrice: getTotalPrice(addedItems),
-                queryId,
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
 
-            }
-            fetch('http://34.82.221.250:8000/web-data', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data)
-            })
-
-        }, [addedItems])
-
-        useEffect(() => {
-            tg.onEvent('mainButtonClicked', onSendData)
-            return () => {
-                tg.offEvent('mainButtonClicked', onSendData)
-            }
-        }, [onSendData]);
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
 
-        if (alreadyAdded){
+        if(alreadyAdded) {
             newItems = addedItems.filter(item => item.id !== product.id);
         } else {
             newItems = [...addedItems, product];
         }
 
         setAddedItems(newItems)
+
         if(newItems.length === 0) {
             tg.MainButton.hide();
         } else {
@@ -70,15 +68,16 @@ const products = [
             })
         }
     }
+
     return (
         <div className={'list'}>
             {products.map(item => (
                 <ProductItem
-                    product = {item}
-                    onAdd = {onAdd}
-                    className = {'item'}
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
                 />
-                ))}
+            ))}
         </div>
     );
 };
